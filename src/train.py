3a9f2e3315084b9337a917443c8b7bd38893176b
prajwal4518 <prajwalpadal17@gmail.com>
2026-02-02 22:08:26 +0530

    Fix MLflow path issue by setting explicit experiment

--- src/train.py
+++ src/train.py
@@ -28,6 +28,9 @@ def train(n_estimators, max_depth):
     # Split into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
+    # Set MLflow experiment
+    mlflow.set_experiment("Titanic_Prediction")
+    
     # Start MLflow run
     with mlflow.start_run():
         # Train model
